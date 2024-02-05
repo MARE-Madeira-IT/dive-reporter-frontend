69c0ea02a5c8560b102ecda150868bec52cf9b3c
FranciscoS00 <53246322+FranciscoS00@users.noreply.github.com>
2024-02-05 17:13:40 +0000

    Text overlapped on image

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -6,27 +6,50 @@ const Container = styled.div`
   margin-left: -21vw;
   overflow: hidden;
   max-height: 100vh;
-  display: block;
+  display: grid;
+  /* Media query for mobile device size */
+`;
+
+const TextContainer = styled.div`
+  grid-area: 1 / 1 / 1 / 4;
+  width: 100vw;
 
   h1 {
+    font-size: 5rem;
+    color: white;
+    line-height: 1;
   }
 
   p {
-    font-size: 50px;
-    color: red;
+    font-size: 2rem;
+    color: white;
+    line-height: 1;
   }
+
+  /* Media query for mobile device size */
 `;
 
 const BackgroundImage = styled.img`
   width: 100%;
   height: auto;
+  grid-area: 1 / 1 / 1 / 4;
+  /* Media query for mobile device size */
 `;
 
 function Home() {
   return (
     <Container>
       <BackgroundImage src="https://i.redd.it/nhk8jg3psng71.jpg" />
-      {/* <p>big teste just to make sure</p> */}
+      <TextContainer>
+        <Row justify={"center"}>
+          <Col xs={24} align="middle">
+            <h1>Dive Reporter</h1>
+          </Col>
+          <Col xs={24} align="middle">
+            <p>A low cost, long term monitoring program</p>
+          </Col>
+        </Row>
+      </TextContainer>
     </Container>
   );
 }
